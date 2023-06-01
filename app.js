e453e3688bb941e2420e7d72fd0808fcce0cd803
Mohammad Tamer Jouni <m.tamer2000.j@gmail.com>
2023-06-01 21:13:43 +0300

    File Upload

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var realEstateRouter = require('./routes/realEstateRouter')
 var favoriteRouter = require('./routes/favoriteRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -54,6 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/realEstate', realEstateRouter)
 app.use('/favorites', favoriteRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
